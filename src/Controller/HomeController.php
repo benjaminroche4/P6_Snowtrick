2e59e2bb6c196f78839a095c98bff7f935bfdc3a
Benjamin Roche <benjamin.roche4@gmail.com>
2021-03-11 10:46:20 +0100

    Modification de FrontController > HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FrontendController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
@@ -14,7 +14,7 @@ class FrontendController extends AbstractController
     public function index(): Response
     {
         return $this->render('frontend/index.html.twig', [
-            'controller_name' => 'FrontendController',
+            'controller_name' => 'HomeController',
         ]);
     }
 
