9f5130b0e53496acc14bdcf2b05597e11c34b5eb
Benjamin Roche <benjamin.roche4@gmail.com>
2021-03-22 14:55:44 +0100

    Ajout du slug dans le l'url pour les figures.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +14,27 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home() {
-        return $this->render('home.html.twig');
+    public function home(){
+        $repo = $this->getDoctrine()->getRepository(Trick::class);
+        $tricks = $repo->findAll();
+
+        return $this->render('home.html.twig', [
+            'controller_name' => 'HomeController',
+            'tricks' => $tricks
+        ]);
     }
+
+    /**
+     * @Route("/trick-{title}", name="trick_show")
+     * @param Trick $trick
+     * @return Response
+     */
+    public function show(Trick $trick):Response{
+        return $this->render('trick.html.twig', [
+            'trick' => $trick
+        ]);
+    }
+
+
 
 }
